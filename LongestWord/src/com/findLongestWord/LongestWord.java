f73f6438a07d2c0116be3f821c8d66dbce3f01ed
PrathiChandra <prathyuch31@gmail.com>
2018-10-21 20:46:14 -0400

    Created unit tests

--- LongestWord/src/com/findLongestWord/LongestWord.java
+++ LongestWord/src/com/findLongestWord/LongestWord.java
@@ -3,25 +3,27 @@ package com.findLongestWord;
 public class LongestWord {
 
 	public String input;
-	
+
 	public Words findLongestWord(String findLongestString) {
-		//Trimming the string if it has space values and handling the only space strings
+		// Trimming the string if it has space values and handling the only space
+		// strings
 		this.input = findLongestString.trim();
-		
-		if(!input.equals(null) && !input.isEmpty()) {
+
+		if (!input.equals(null) && !input.isEmpty()) {
 			Words words = new Words();
-			//Splitting the string with space value to get words into a string array
+			// Splitting the string with space value to get words into a string array
 			String[] splitStr = input.split(" ");
-			
+
 			words.length = 0;
-			for(int i=0; i<splitStr.length; i++) {
-				int tempLength = splitStr[i].length(); 
-				if(tempLength >= words.length) {
-					/*Checking if the new string in the loop has a greater length than before length.
-					 * If the condition is satisfied we are clearing the array List 
-					 * to add new highly added values.
+			for (int i = 0; i < splitStr.length; i++) {
+				int tempLength = splitStr[i].length();
+				if (tempLength >= words.length) {
+					/*
+					 * Checking if the new string in the loop has a greater length than before
+					 * length. If the condition is satisfied we are clearing the array List to add
+					 * new highly added values.
 					 */
-					if(words.length < tempLength) {
+					if (words.length < tempLength) {
 						words.longestWords.clear();
 					}
 					words.length = tempLength;
--- LongestWord/src/com/findLongestWord/Words.java
+++ LongestWord/src/com/findLongestWord/Words.java
@@ -1,20 +1,24 @@
 package com.findLongestWord;
+
 import java.util.ArrayList;
 
 public class Words {
 
 	int length;
 	ArrayList<String> longestWords = new ArrayList<String>();
-	
+
 	public int getLength() {
 		return length;
 	}
+
 	public void setLength(int length) {
 		this.length = length;
 	}
+
 	public ArrayList<String> getLongestWords() {
 		return longestWords;
 	}
+
 	public void setLongestWords(ArrayList<String> longestWords) {
 		this.longestWords = longestWords;
 	}
